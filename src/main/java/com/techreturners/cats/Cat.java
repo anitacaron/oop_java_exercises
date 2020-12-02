0b8f77cb02f1c436c89e7dd7e903e0c3a70378b0
anitacaron <annycaron@gmail.com>
2020-12-02 19:00:22 +0000

    Update Cat.java

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -1,5 +1,44 @@
 package com.techreturners.cats;
 
 interface Cat {
+	public String eat();
+	public boolean isAsleep();
+	public void wakeUp();
+	public void goToSleep();
+
+}
+
+
+/*Domestic*/
+class DomesticCat implements Cat {
+
+	private boolean isAsleep = false;
+	private float avgHeight;
+	private String setting = "domestic";
+
+	public DomesticCat(float avgHeight, String setting) {
+		this.avgHeight = avgHeight;
+		this.setting = setting;
+	}
+
+	public boolean isAsleep() {
+	    return isAsleep;
+	}
+
+	public void goToSleep() {
+		isAsleep = true;
+	}
+
+	public String eat() {        
+	    return "Purrrrrrr";
+	}
+
+	public void wakeUp() {
+	    isAsleep = false; 
+	}
+
+	public String getSetting() {
+		return setting;
+	}
 
 }
